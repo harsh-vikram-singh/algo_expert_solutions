5274ce2082c027d35b13e6a88d2b4adc90fc2f50
Harsh Vikram Singh <singh.harshvikram@gmail.com>
2020-11-16 12:35:54 +0530

    added a second solution that implements the depth first search without a helper function

--- graphs/depth_first_search/index.js
+++ graphs/depth_first_search/index.js
@@ -10,32 +10,48 @@ class Node {
   }
 
   depthFirstSearch(array) {
-    const recursor = (node) => {
-      array.push(node.name);
-      if (node.children.length === 0) {
-        return;
-      } else {
-        for (let child of node.children) {
-          recursor(child);
-        }
+    // Solution 1: using the helper function recursion
+    // const recursor = (node) => {
+    //   array.push(node.name);
+    //   if (node.children.length === 0) {
+    //     return;
+    //   } else {
+    //     for (let child of node.children) {
+    //       recursor(child);
+    //     }
+    //   }
+    // }
+    // recursor(this);
+		// // console.log(array);
+    // return array;
+
+    // Solution 2: without a helper function
+    if (array === undefined) {
+      array = [];
+    }
+    console.log(`called on node: ${this.value} with array: ${array}`)
+    array.push(this.name);
+    if (this.children.length > 0) {
+      for (let child of this.children) {
+        child.depthFirstSearch(array);
       }
     }
-    recursor(this);
-		// console.log(array);
     return array;
+    // note: even though this value is not captured, since variables
+    // point to the same location in memory, they get appended to the same array
   }
 }
 
 // test code
-// let a = new Node('a');
-// a.addChild('b');
-// a.addChild('c');
-// a.addChild('d');
-// a.children[0].addChild('e');
-// a.children[0].addChild('f');
-// a.children[2].addChild('g');
-// a.children[2].addChild('h');
-// a.children[0].children[1].addChild('i');
-// a.children[0].children[1].addChild('j');
-// a.children[2].children[0].addChild('k');
-// console.log(a.depthFirstSearch());
+let a = new Node('a');
+a.addChild('b');
+a.addChild('c');
+a.addChild('d');
+a.children[0].addChild('e');
+a.children[0].addChild('f');
+a.children[2].addChild('g');
+a.children[2].addChild('h');
+a.children[0].children[1].addChild('i');
+a.children[0].children[1].addChild('j');
+a.children[2].children[0].addChild('k');
+console.log(a.depthFirstSearch());
